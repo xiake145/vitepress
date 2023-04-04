e87e05fa30d9bfe73b757cf1dcf7fc57564f6c52
xuhuayu <xuhuayu@jindiandan.cn>
2023-04-04 19:45:00 +0800

    配置修改

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: '侠客的博客',
   description: '记录学习日常',
   lang: 'zh-cn',
-  base: '/vitepress/',
+  base: process.env.NODE_ENV === 'production' ? '/vitepress/' : '/',
   themeConfig: {
     nav: [
       { text: 'CSS', link: '/CSS' },
